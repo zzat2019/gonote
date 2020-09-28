e0c708a39a319bf4a7e06428c74af19eadfb40f3
zzat2019 <zhaozhe_2018@163.com>
2020-09-28 18:41:35 +0800

    断言，子测试

--- hello_test.go
+++ hello_test.go
@@ -14,14 +14,26 @@ func TestHello(t *testing.T) {
 	}
 	//子测试
 	t.Run("say hello to people", func(t *testing.T) {
-		got := Hello("Chris")
+		got := Hello("Chris", "")
 		want := "Hello Chris"
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("say hello world when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello World"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Elodie", "French")
+		want := "Bonjour Elodie"
 		assertCorrectMessage(t, got, want)
 	})
 }
