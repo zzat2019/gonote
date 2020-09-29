7f578bcb684e68ee137b23d8330d8c5dad9bef00
zzat2019 <zhaozhe_2018@163.com>
2020-09-29 15:49:38 +0800

    表格驱动测试

--- perimeter/perimeter.go
+++ perimeter/perimeter.go
@@ -14,6 +14,11 @@ type Circle struct {
 	Radius float64
 }
 
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
 func Perimeter(rectangle Rectangle) float64 {
 	return 2 * (rectangle.Width + rectangle.Height)
 }
@@ -30,4 +35,8 @@ func (r Rectangle) Area() float64 {
 
 func (c Circle) Area() float64 {
 	return math.Pi * c.Radius * c.Radius
+}
+
+func (c Triangle) Area() float64 {
+	return (c.Base * c.Height) * 0.5
 }
