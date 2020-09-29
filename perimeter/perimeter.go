755f02e657b6ccc4537aa55c218ffd04bb0c747a
zzat2019 <zhaozhe_2018@163.com>
2020-09-29 15:38:31 +0800

    结构体，接口

---
+++ perimeter/perimeter.go
@@ -0,0 +1,33 @@
+package perimeter
+
+import "math"
+
+type Shape interface {
+	Area() float64
+}
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Width + rectangle.Height)
+}
+
+//函数
+func Area(rectangle Rectangle) float64 {
+	return rectangle.Width * rectangle.Height
+}
+
+//方法
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
