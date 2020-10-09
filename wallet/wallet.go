1537ca54faecd00312ad20202253e758602923f8
zzat2019 <zhaozhe_2018@163.com>
2020-10-09 15:33:59 +0800

    表格驱动测试

---
+++ wallet/wallet.go
@@ -0,0 +1,37 @@
+package wallet
+
+import (
+	"errors"
+	"fmt"
+)
+
+type Bitcoin int
+type Wallet struct {
+	balance Bitcoin
+}
+type Stringer interface {
+	String() string
+}
+
+func (w *Wallet) Deposit(amount Bitcoin) {
+	w.balance += amount
+}
+
+func (w *Wallet) Balance() Bitcoin {
+	return w.balance
+}
+
+var InsufficientFundsError = errors.New("cannot withdraw, insufficient funds")
+
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+	if amount > w.balance {
+		return InsufficientFundsError
+	}
+
+	w.balance -= amount
+	return nil
+}
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
